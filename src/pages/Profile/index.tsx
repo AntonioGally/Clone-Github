c063fac0b2a5608c9c4908f9ef398c84060d75b0
ANTONIO LIMA GALLY NETO <antonio.gally@etec.sp.gov.br>
2020-08-08 23:49:32 -0300

    adicionando header

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+const Profile: React.FC = () => {
+  return (
+    <Container>
+      <Main>
+        <LeftSide></LeftSide>
+
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
+}
+
+export default Profile;
